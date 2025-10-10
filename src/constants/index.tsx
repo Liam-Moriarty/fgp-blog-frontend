f9350eb8fa088ac4938f394ecde3b2756cfae279
Fernando Ordiales <fernando.ordiales23@gmail.com>
2025-10-10 10:48:22 +0800

    add accordion of shadcn

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,4 +1,12 @@
-import type { Engagement, NavLink } from "@/interfaces";
+import type { CoreValues, Engagement, NavLink } from "@/interfaces";
+
+import {
+  LuFolderOpen,
+  LuFiles,
+  LuFlag,
+  LuChartColumnIncreasing,
+  LuImage,
+} from "react-icons/lu";
 
 export const navLinks: NavLink[] = [
   { path: "/", label: "Home" },
@@ -12,4 +20,36 @@ export const engagementStats: Engagement[] = [
   { label: "Total Visitors", count: 235 },
   { label: "Today's Views", count: 52 },
   { label: "Today's Visitors", count: 21 },
+];
+
+export const coreValues: CoreValues[] = [
+  { title: "Core Values" },
+  {
+    icon: LuFolderOpen,
+    title: "Integrity",
+    description:
+      "Building trust through ethical and transparent business practices.",
+  },
+  {
+    icon: LuFiles,
+    title: "Professionalism",
+    description: "Delivering excellence at every stage of operation.",
+  },
+  {
+    icon: LuFlag,
+    title: "Innovation",
+    description:
+      "Creating cutting-edge solutions that anticipate future needs.",
+  },
+  {
+    icon: LuChartColumnIncreasing,
+    title: "Collaboration",
+    description: "Empowering partnerships that achieve shared success.",
+  },
+  {
+    icon: LuImage,
+    title: "Sustainability",
+    description:
+      "Investing in projects that benefit both economy and environment.",
+  },
 ];
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,5 @@
+import type { IconType } from "react-icons";
+
 export interface NavLink {
   path: string;
   label: string;
@@ -6,4 +8,10 @@ export interface NavLink {
 export interface Engagement {
   label: string;
   count: number;
+}
+
+export interface CoreValues {
+  icon?: IconType;
+  title: string;
+  description?: string;
 }
